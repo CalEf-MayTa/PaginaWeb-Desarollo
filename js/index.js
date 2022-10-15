01db05ffe6eaa09b635bee8cdf4d57d83d93d235
CalEf-MayTa <cal_12_96@hotmail.com>
2022-10-14 23:17:54 -0500

    versionViernes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
-const menu = document.getElementById('navIconMenu')
-const nav = document.querySelector('.nav')
+'use strict';
 
-menu.addEventListener('click', () => {
-    nav.classList.toggle('nav--open')
-})
-// --menu: document
+document.addEventListener('click', () => {
+    const header = document.querySelector('.header');
+    const nav = document.querySelector('.nav');
+    const navIconMenu = document.getElementById('navIconMenu');
+    const navIconClose = document.querySelector('#navIconClose');
+    const menu = document.querySelectorAll('.menu__link');
+
+    const closeMenu = () => {
+        nav.classList.remove('nav--open')    
+    };
+
+    document.addEventListener('scroll', () => {
+        header.classList.toggle('header--scroll', scrollY > 0);
+        nav.classList.toggle('nav--scroll', scrollY > 0);
+    });
+
+    navIconMenu.addEventListener('click', () => {
+        nav.classList.add('nav--open');
+    });
+
+    navIconClose.addEventListener('click', closeMenu);
+
+    menuLinks.forEach((element) => {
+        element.addEventListener('click', closeMenu);
+    });
+
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        speed: 500,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000
+        }
+    });
+});
+
